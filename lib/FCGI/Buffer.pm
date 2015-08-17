dbcfaafa0ffa68cbd0cd9ab59b376c1dfa61c970
Nigel Horne <njh@bandsman.co.uk>
2015-08-16 20:19:40 -0400

    Release 0.05

--- lib/FCGI/Buffer.pm
+++ lib/FCGI/Buffer.pm
@@ -17,11 +17,11 @@ FCGI::Buffer - Verify, Cache and Optimise FCGI Output
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
