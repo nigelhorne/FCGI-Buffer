eb030a97e852d53502bcfaaebf86f84e3b04b009
Nigel Horne <njh@bandsman.co.uk>
2016-10-14 14:49:20 -0400

    Fix typo

--- lib/FCGI/Buffer.pm
+++ lib/FCGI/Buffer.pm
@@ -1282,7 +1282,7 @@ L<http://search.cpan.org/dist/FCGI-Buffer/>
 
 =head1 ACKNOWLEDGEMENTS
 
-The inspiration and code for some io this is cgi_buffer by Mark
+The inspiration and code for some of this is cgi_buffer by Mark
 Nottingham: http://www.mnot.net/cgi_buffer.
 
 =head1 LICENSE AND COPYRIGHT
