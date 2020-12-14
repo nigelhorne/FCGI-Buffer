b8fbe80c0b22f5b016712a91f75de2b9af15e9e2
Nigel Horne <nigel.horne@nasa.gov>
2020-12-14 08:58:11 -0500

    Easier to read

--- lib/FCGI/Buffer.pm
+++ lib/FCGI/Buffer.pm
@@ -402,7 +402,7 @@ sub DESTROY {
 						push @{$self->{o}}, 'X-Cache-Lookup: HIT';
 					}
 				} else {
-					carp __PACKAGE__, ": error retrieving data for key $key";
+					carp( __PACKAGE__, ": error retrieving data for key $key");
 				}
 			}
 
